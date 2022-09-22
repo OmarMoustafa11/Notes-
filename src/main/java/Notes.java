a79c170b83b49dafe64e189859558d03b1585bc2
omarmoustafa11 <omaroj99@yahoo.com>
2022-09-22 10:26:36 -0400

    sorry didnt see your slack message

---
+++ src/main/java/Notes.java
@@ -0,0 +1,47 @@
+public class Notes {
+    /*
+    Polymorphism means many forms
+    when creating a polymorphism class you can have subclasses that only create
+    overrides for the super class or (parent class).
+    Casting
+    in the example on pg216 they use dog
+     Object o = al.get(index);
+     Dog d = (Dog) o; casting the object back to a dog that we know is there
+     d.roam();
+     if (o instanceof Dog) {
+     Dog d = (Dog) o; }
+
+     INTERFACE
+     Interace class is a 100% abstract class
+     what is abstract
+     Abstract is a superclass that cannot be instantiated and is used
+     to state or define general characteristics.
+
+      INSTANCE AND LOCAL VARIABLES
+      INSTANCE are declared inside a class but not inside a method
+      LOCAL declared inside a method, including method parameters
+
+
+        **Page 47**
+
+       ** Page 72**
+       what are objects and how are they declared in instance variables
+       the new keyword is used to create new objects. Declaration − A variable declaration with
+       a variable name with an object type. Instantiation − The 'new' keyword is used to create the object.
+
+           www.tutorialspoint.com › java › java_object_classes
+           A static method is a method that belongs to a class, but it does not belong to an instance of that class and this method can be called without the instance or object of that class.
+           Every method in java defaults to a non-static method without static keyword preceding it.
+           Non-static methods can access any static method and static variable,
+            without creating an instance of the object. Let us clarify the differences Below are the various important differences among these pointers as follows:
+
+        Accessing members and methods
+        Calling process
+        Binding process
+        Overriding process
+        Memory allocation
+        https://www.geeksforgeeks.org/difference-between-static-and-non-static-method-in-java/
+     */
+
+
+}
